9f7a40011f7d02dcbca34cfce50803372b876a9e
Gin Elmartian <duadelapanmaret@gmail.com>
2014-05-04 22:35:56 +0700

    merubah warna dan memperbaiki kalimat
    merubah warna #FF0066 menjadi #FF0000
    memperbaiki kalimat yang kurang tepat

--- pengalaman.php
+++ pengalaman.php
@@ -4,14 +4,14 @@
   <tr>
     <td width="171" height="332" valign="top" bordercolor="#000000" background="images/bg1.JPG">
 	<?php include("nav.php"); ?>
-    <td width="581" valign="top" border="2" background="images/bg3.JPG" bgcolor="#FF0066">
+    <td width="581" valign="top" border="2" background="images/bg3.JPG" bgcolor="#FF0000">
 	<div align="center"><br/>
 	  <span style="font-weight: bold">Salah Arah Kiblat</span>	</div>
 	<table width="518" height="318" border="0" align="center">
       <tr>
         <td>
-          <p style="font-family: Georgia, &quot;Times New Roman&quot;, Times, serif; font-size: 12px"> cerita saat herei dengan teman2 di batara kami terinvirasi untuk membuat sebuah keterampilan di bidangnya dengan membuatsebuah website yang ala kandarnya</p>
-          <p style="font-family: Georgia, &quot;Times New Roman&quot;, Times, serif; font-size: 12px">ketika melihat sebuah gugulayunan dan sebuah tangkal yang sukar di taeki olleh barudak batara haha &quot;ehh,, terinvirasi&quot; lalu saya betaya pada barudak arek wang ngieun website &quot;, kumaha carana,, &quot;. </p>
+          <p style="font-family: Georgia, &quot;Times New Roman&quot;, Times, serif; font-size: 12px">cerita saat hereuy dengan teman2 di batara kami terinvirasi untuk membuat sebuah keterampilan di bidangnya dengan membuat sebuah website yang ala kadarnya</p>
+          <p style="font-family: Georgia, &quot;Times New Roman&quot;, Times, serif; font-size: 12px">ketika melihat sebuah gugulayunan dan sebuah tangkal yang sukar di taeki olleh barudak batara haha &quot;ehh,, terinvirasi&quot; lalu saya bertanya pada barudak arek wang nyieun website &quot;, kumaha carana?,, &quot;. </p>
           <p style="font-family: Georgia, &quot;Times New Roman&quot;, Times, serif; font-size: 12px">sakiw jawab aya w,,</p>
           <p>&nbsp;</p></td>
       </tr>
