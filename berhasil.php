3403c67584e1ca77e2aa8808c47877d467b5dd8e
Gin Elmartian <duadelapanmaret@gmail.com>
2014-05-04 22:19:14 +0700

    merubah warna dan menambahkan tanda seru
    merubah warna bgcolor ="FF0066" menjadi "FF0000"
    dan menambahkan tanda seru di kalimat "Pendaftaran Berhasil, Silahkan login"

--- berhasil.php
+++ berhasil.php
@@ -3,9 +3,9 @@
   <tr>
     <td width="171" height="332" valign="top" bordercolor="#000000" background="images/bg1.JPG">
 	<?php include("nav.php"); ?>
-    <td width="581" valign="top" border="2" background="images/bg3.JPG" bgcolor="#FF0066">
+    <td width="581" valign="top" border="2" background="images/bg3.JPG" bgcolor="#FF0000">
 	<br/>
-	<p align="center" style="font-weight: bold"><blink>Pendaftaran Berhasil, Silahkan login</blink></p>
+	<p align="center" style="font-weight: bold"><blink>Pendaftaran Berhasil, Silahkan login!!!</blink></p>
 	
 
   </tr>
